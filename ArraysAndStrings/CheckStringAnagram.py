f86c57d6eaf74628c840e96b63ba90637efa4262
Seo <jeff.jh.seo@gmail.com>
2016-05-30 23:46:21 -0700

    fix 1.4

--- ArraysAndStrings/CheckStringAnagram.py
+++ ArraysAndStrings/CheckStringAnagram.py
@@ -4,11 +4,15 @@
 
 def is_string_anagram(string1,string2):
     if isinstance(string1,str) and isinstance(string2,str):      
-        if len(set(string1)) == len(set(string2)):
-            return True
-        else:
-            return False
+        return sorted(string1) == sorted(string2)
+
+def is_string_anagram_v2(string1,string2):
+    if sum(ord(x) for x in string1) == sum(ord(x) for x in string2):
+        return True
+    else:
+        return False
 
 if __name__ == '__main__':
     print is_string_anagram('iceman','cinema')
+    print is_string_anagram_v2('iceman','cinema')
 
