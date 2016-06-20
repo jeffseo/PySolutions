c93931abd16010f4b6805ee28271c39ef0dfc73a
Seo <jeff.jh.seo@gmail.com>
2016-06-19 20:27:06 -0700

    3.1 todo

---
+++ StacksAndQueues/SingleArrayThreeStacks.py
@@ -0,0 +1,11 @@
+"""
+3.1
+Describe how you could use a single array to implement three stacks
+
+Stacks = LIFO (Last in, First Out)
+Operations include: push, pop, peek, isEmpty
+Requires: a pointer to track the top
+
+1: split the array into 3
+[0,n/3) [n/3,2n/3) [2n/3,n)
+"""
