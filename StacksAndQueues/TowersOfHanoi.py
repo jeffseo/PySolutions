b7bd8d58ece76fc8c647b29bc6c655e29650e0a7
jfseo <jhseo.sk@gmail.com>
2016-09-21 01:50:22 -0700

    TowersOfHanoi working?

--- StacksAndQueues/TowersOfHanoi.py
+++ StacksAndQueues/TowersOfHanoi.py
@@ -22,13 +22,60 @@ class Stack(object):
     def peek(self):
         return self.stackList[-1]
 
+    def isEmpty(self):
+        return len(self.stackList) == 0
+
     def __len__(self):
         return len(self.stackList)
 
     def __str__(self):
         return str(self.stackList)
 
-class Tower(object):
+class TowerOfHanoi(object):
     def __init__(self):
-        self.rodList = [Stack(), Stack(), Stack()]
-        
+        self.pegList = [Stack(), Stack(), Stack()]
+        self.numOfDisks = 0
+
+    def add(self,disk):
+        if not self.pegList[0].isEmpty() and self.pegList[0].peek() <= disk:
+            print "Error placing disk" + str(disk)
+        else:
+            self.pegList[0].push(disk)
+            self.numOfDisks += 1
+
+"""
+To move disks from peg A to C
+1. move (n-1) disk from peg A to B using C and intermediate Peg
+2. move Nth disk from peg A to C
+3. move (n-1) disk from B to C using A as intermediate peg
+T(N) = T(N-1) + 1 + T(N-1) = 2T(N-1) + 1 // recurrence relation
+T(N) = (2^N) - 1 
+T(N) = # of steps required to move disks from src to dst
+
+hanoi(N, source, destination, auxilary)
+"""
+
+    def move(self, n, src, dest, aux):
+        if n > 1:
+            self.move(n - 1, src, aux, dest)
+            self.move(1, src, dest, aux)
+            self.move(n-1, aux, dest, src)
+        else:
+            self.pegList[dest].push(self.pegList[src].pop())
+            print src, "->", dest
+
+    # call move to start the computation of recursive solution
+    def hanoi(self, h):
+        for i in range(h,0,-1): #goes backwards 3,2,1
+            self.add(i)
+        print '0:',self.pegList[0]
+        print '1:',self.pegList[1]
+        print '2:',self.pegList[2]
+        self.move(h, 0, 2, 1) #0, 1, 2 refers to each stack in the pegList
+        print '0:',self.pegList[0]
+        print '1:',self.pegList[1]
+        print '2:',self.pegList[2]
+
+if __name__ == '__main__':
+    test = TowerOfHanoi()
+    test.hanoi(3)
