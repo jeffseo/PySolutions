a4db5c738b6b12f18e4482a0b1c3e4fd88439303
Jeff Seo <jhseo.sk@gmail.com>
2016-09-21 20:17:01 -0700

    wip

--- TreesAndGraphs/BalancedTree.py
+++ TreesAndGraphs/BalancedTree.py
@@ -4,3 +4,43 @@ a balanced tree is defined to be a tree such that no two leaf nodes differ in di
 from the root by more than one.
 """
 
+"""
+Brute-force: Iteratively go through each node of tree and count number of steps of each leaf nodes.
+Thinking about it, really only need to check the difference between the longest leaf node and shortest.
+"""
+
+class TreeNode(object):
+    def __init__(self, data):
+        self.data = data
+        self.leftChild = None
+        self.rightChild = None
+
+    def IsLeaf(self):
+        return self.leftChild == None and self.rightChild == None
+
+    def insert(self,node):
+        if node.data > self.data:
+            if self.right is None:
+                self.right = node
+            else:
+                self.right.insert(node)
+        elif node.data <= self.data:
+            if self.left is None:
+                self.left = node
+            else:
+                self.left.insert(node)
+        
+def maxDepth(self, root):
+    if (root == None):
+        return 0
+    return 1 + max(maxDepth(root.leftChild), maxDepth(root.rightChild))
+
+def minDepth(self, root):
+    if (root == None):
+        return 0
+    return 1 + min(minDepth(root.leftChild), minDepth(root.rightChild))
+
+def isBalanced(self):
+    return (self.maxDepth() - self.minDepth() <= 1)
+
+if __name__ == '__main__':
