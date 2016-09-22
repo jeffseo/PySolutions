149708018f1834dca8e2126b6c3f63daf7debab2
jfseo <jhseo.sk@gmail.com>
2016-09-22 01:14:11 -0700

    4.4 WIP

---
+++ TreesAndGraphs/BSTToLinkedLists.py
@@ -0,0 +1,94 @@
+"""
+4.4
+Given a binary search tree, design an algorithm which creates a linked list of all the
+nodes at each depth (eg, if you have a tree with depth D, you’ll have D linked lists).
+
+Since we want all nodes at each depth, want to use BFS method.
+BFS uses a queue to track nodes.
+
+"""
+
+class TreeNode(object):
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+def createLinkedList(root):
+    level = 0
+    result = []
+    newList = UnorderedList()
+    newList.add(root)
+    result.add(level, newList)
+    while True:
+        newSubList = UnorderedList()
+        for i in result[level].size():
+            
+
+class Node:
+    def __init__(self,initdata):
+        self.data = initdata
+        self.next = None
+
+    def getData(self):
+        return self.data
+
+    def getNext(self):
+        return self.next
+
+    def setData(self,newdata):
+        self.data = newdata
+
+    def setNext(self,newnext):
+        self.next = newnext
+
+class UnorderedList:
+    def __init__(self):
+        self.head = None
+
+    def isEmpty(self):
+        return self.head == None
+
+    def add(self,item):
+        temp = Node(item)
+        temp.setNext(self.head)
+        self.head = temp
+
+    def size(self):
+        current = self.head
+        count = 0
+        while current != None:
+            count = count + 1
+            current = current.getNext()
+
+        return count
+
+    def search(self,item):
+        current = self.head
+        found = False
+        while current != None and not found:
+            if current.getData() == item:
+                found = True
+            else:
+                current = current.getNext()
+
+        return found
+
+    def remove(self,item):
+        current = self.head
+        previous = None
+        found = False
+        while not found:
+            if current.getData() == item:
+                found = True
+            else:
+                previous = current
+                current = current.getNext()
+
+        if previous == None:
+            self.head = current.getNext()
+        else:
+            previous.setNext(current.getNext())
+
+if __name__ == '__main__':
+    pass
