fb94b0458e795988216c870b6798ebf9f3f7a4f0
jfseo <jhseo.sk@gmail.com>
2016-09-23 01:01:17 -0700

    added iterative traversals

--- TreesAndGraphs/Traversal.py
+++ TreesAndGraphs/Traversal.py
@@ -66,6 +66,145 @@ def BreadthFirstSearch(root, searchValue):
             print node.data
     return False
 
+def peek(stack):
+    if len(stack) > 0:
+        return stack[-1]
+    return None
+
+# Iterative function for inorder tree traversal
+def inOrder(root):
+     
+    # Set current to root of binary tree
+    current = root 
+    s = [] # initialze stack
+    done = 0
+     
+    while(not done):
+         
+        # Reach the left most Node of the current Node
+        if current is not None:
+             
+            # Place pointer to a tree node on the stack 
+            # before traversing the node's left subtree
+            s.append(current)
+         
+            current = current.left 
+ 
+         
+        # BackTrack from the empty subtree and visit the Node
+        # at the top of the stack; however, if the stack is 
+        # empty you are done
+        else:
+            if(len(s) >0 ):
+                current = s.pop()
+                print current.data,
+         
+                # We have visited the node and its left 
+                # subtree. Now, it's right subtree's turn
+                current = current.right 
+ 
+            else:
+                done = 1
+
+# An iterative process to print preorder traveral of BT
+# pre-order (root, left, right)
+def iterativePreorder(root):
+     
+    # Base CAse 
+    if root is None:
+        return
+ 
+    # create an empty stack and push root to it
+    nodeStack = []
+    nodeStack.append(root)
+ 
+    #  Pop all items one by one. Do following for every popped item
+    #   a) print it
+    #   b) push its right child
+    #   c) push its left child
+    # Note that right child is pushed first so that left
+    # is processed first */
+    while(len(nodeStack) > 0):
+         
+        # Pop the top item from stack and print it
+        node = nodeStack.pop()
+        print node.data,
+         
+        # Push right and left children of the popped node
+        # to stack
+        if node.right is not None:
+            nodeStack.append(node.right)
+        if node.left is not None:
+            nodeStack.append(node.left)
+
+# An iterative function to do postorder traversal of a
+# given binary tree using 2 stacks
+def postOrderIterative2(root): 
+ 
+    if root is None:
+        return         
+     
+    # Create two stacks 
+    s1 = []
+    s2 = []
+     
+    # Push root to first stack
+    s1.append(root)
+     
+    # Run while first stack is not empty
+    while (len(s1) >0):
+         
+        # Pop an item from s1 and append it to s2
+        node = s1.pop()
+        s2.append(node)
+     
+        # Push left and right children of removed item to s1
+        if node.left is not None:
+            s1.append(node.left)
+        if node.right is not None :
+            s1.append(node.right)
+ 
+        # Print all eleements of second stack
+    while(len(s2) > 0):
+        node = s2.pop()
+        print node.data,
+
+def postOrderIterative(root):
+    if root is None:
+        return
+    ans = []
+    stack = []
+
+    while True:
+        while root:
+            # Push root's right child and then root to stack
+            if root.right is not None:
+                stack.append(root.right)
+            stack.append(root)
+
+            # Set root as root's left child
+            root = root.left
+
+        # Pop an item from stack and set it as root
+        root = stack.pop()
+ 
+        # If the popped item has a right child and the
+        # right child is not processed yet, then make sure
+        # right child is processed before root
+        if (root.right is not None and peek(stack) == root.right):
+            stack.pop() # Remove right child from stack 
+            stack.append(root) # Push root back to stack
+            root = root.right # change root so that the 
+                             # righ childis processed next
+ 
+        # Else print root's data and set root as None
+        else:
+            ans.append(root.data) 
+            root = None
+ 
+        if (len(stack) <= 0):
+            return ans
+
 def PostOrderTraversal(root):
     if root == None:
         return
@@ -73,23 +212,24 @@ def PostOrderTraversal(root):
     PostOrderTraversal(root.right)
     print root.data
 
+#WIP: This is not DFS
 # NOTE: DFS on a tree is equivalent to post-order traversal
 def DepthFirstSearch(root):
     print 'DFS:'
-    PostOrderTraversal(root)
+    #return PostOrderTraversal(root)
 
-    # tracker = Stack()
-    # tracker.push(root)
+    visited = []
+    tracker = Stack()
+    tracker.push(root)
 
-    # while tracker.size() != 0:
-    #     node = tracker.pop()
-    #     if node.right != None and node.right not in visited:
-    #         tracker.push(root.right)
-    #         visited.append(node.right)
-    #     if node.left != None and node.left not in visited:
-    #         tracker.push(root.left)
-    #         visited.append(node.left)
-    #     print node.data
+    while tracker.size() != 0:
+        node = tracker.pop()
+        visited.append(node)
+        if node.right != None:
+            tracker.push(node.right)
+        if node.left != None:
+            tracker.push(node.left)
+    return visited
 
 if __name__ == "__main__":
     print "\n\
@@ -97,7 +237,7 @@ if __name__ == "__main__":
           / \\      \n\
          2   3      \n\
         / \\        \n\
-       4  5"
+       4   5"
     root = TreeNode(1)
     root.left = TreeNode(2)
     root.right = TreeNode(3)
@@ -106,7 +246,15 @@ if __name__ == "__main__":
     root.left.right = TreeNode(5)
 
     BreadthFirstSearch(root, 6)
-    DepthFirstSearch(root)
+
+    result = []
+    for i in DepthFirstSearch(root):
+        result.append(i.data)
+    print result
+
+    print "Post Order traversal of binary tree is"
+    print postOrderIterative(root)
+
 
 
 
